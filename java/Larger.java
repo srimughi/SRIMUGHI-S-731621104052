d852ee470c020e8037e99b1233015aa63dbda111
Srimughi <ssrimughi@gmail.com>
2024-08-10 17:13:57 +0530

    java prac1

---
+++ java/Larger.java
@@ -0,0 +1,18 @@
+
+public class Larger{
+ public static void main(String args[]){
+int num1=50;
+int num2=100;
+if(num1>num2){
+System.out.print(num1 + "is greater");
+}
+else{
+System.out.println(num2 + "is greater");
+}
+}
+}
+
+
+
+
+
---
+++ java/MyClass.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class MyClass {
+  public static void main(String args[]) {
+    int employeeId;
+    String employeeName;
+    float employeeSalary;
+   
+    Scanner sc=new Scanner(System.in);
+
+    System.out.println("Enter employee id");
+    employeeId=sc.nextInt();
+    System.out.println("Enter employee name");
+    employeeName=sc.next();
+    System.out.println("Enter employee salary");
+    employeeSalary=sc.nextFloat();
+System.out.println("Employee id :" + employeeId);
+System.out.println("Employee name :" + employeeName);
+System.out.println("Employee salary :" + employeeSalary);
+
+}
+}
